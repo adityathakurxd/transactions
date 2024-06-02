c7f916fbd69fdc4cb7883ce52f97ffedf86677b1
Aditya Thakur <adityathakurxd@gmail.com>
2024-06-02 14:17:57 +0530

    Should fix for mobile devices using horizontal scroll

--- lib/screens/transaction_screen.dart
+++ lib/screens/transaction_screen.dart
@@ -14,7 +14,7 @@ class TransactionScreen extends StatelessWidget {
         final AsyncValue transactions = ref.watch(transactionProvider);
         return Center(
           child: switch (transactions) {
-            AsyncData(:final value) => TransactionsGrid(transactions: value),
+            AsyncData(:final value) => TransactionWidget(transactions: value),
             AsyncError(:final error) => Scaffold(
                 body: Text('Oops, something unexpected happened $error'),
               ),
@@ -26,10 +26,10 @@ class TransactionScreen extends StatelessWidget {
   }
 }
 
-class TransactionsGrid extends StatelessWidget {
+class TransactionWidget extends StatelessWidget {
   final List<DailyTransactions> transactions;
 
-  const TransactionsGrid({super.key, required this.transactions});
+  const TransactionWidget({super.key, required this.transactions});
 
   @override
   Widget build(BuildContext context) {
@@ -46,99 +46,50 @@ class TransactionsGrid extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 14, 17, 23),
-      body: Center(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: monthHeaders.expand((month) {
-                  return [
-                    Text(
-                      month,
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                    const Spacer(),
-                  ];
-                }).toList(),
-              ),
-              const SizedBox(height: 5),
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: columns,
-                    crossAxisSpacing: 2.0,
-                    mainAxisSpacing: 2.0,
-                  ),
-                  itemCount: reversedTransactions.length,
-                  itemBuilder: (context, index) {
-                    int column = index % columns;
-                    int row = index ~/ columns;
-                    int actualIndex = row + column * rows;
-
-                    if (actualIndex >= reversedTransactions.length) {
-                      return Container();
-                    }
-
-                    Color color = _getColorForSpending(
-                        reversedTransactions[actualIndex].totalAmountSpent,
-                        maxSpending);
-
-                    return GestureDetector(
-                      onTap: () {
-                        if (reversedTransactions[actualIndex]
-                                .transactions
-                                .length >
-                            1) {
-                          showModalBottomSheet(
-                            context: context,
-                            builder: (context) {
-                              return ListView.builder(
-                                itemCount: reversedTransactions[actualIndex]
-                                    .transactions
-                                    .length,
-                                itemBuilder: (context, innerIndex) {
-                                  Transaction transaction =
-                                      reversedTransactions[actualIndex]
-                                          .transactions[innerIndex];
-                                  return ListTile(
-                                    title: Text(transaction.category),
-                                    subtitle: Row(
-                                      children: [
-                                        transaction.isCredit
-                                            ? Container()
-                                            : const Text("-"),
-                                        Text(transaction.amount
-                                            .toStringAsFixed(2)),
-                                      ],
-                                    ),
-                                    trailing: Text(DateFormat.yMMMMd().format(
-                                        reversedTransactions[actualIndex]
-                                            .date)),
-                                  );
-                                },
-                              );
-                            },
-                          );
-                        } else {
-                          ScaffoldMessenger.of(context)
-                              .showSnackBar(const SnackBar(
-                            content:
-                                Text("No transaction available at the moment"),
-                          ));
-                        }
-                      },
-                      child: Container(
-                        color: color,
-                        margin: const EdgeInsets.all(2.0),
+          child: SizedBox(
+            width: 1000,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 150,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        height: 10,
                       ),
-                    );
-                  },
+                      Text("Mon", style: TextStyle(color: Colors.white)),
+                      Text("Wed", style: TextStyle(color: Colors.white)),
+                      Text("Fri", style: TextStyle(color: Colors.white)),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      MonthHeaderWidget(monthHeaders: monthHeaders),
+                      const SizedBox(height: 5),
+                      Expanded(
+                          child: TransactionGrid(
+                        rows: rows,
+                        columns: columns,
+                        reversedTransactions: reversedTransactions,
+                        maxSpending: maxSpending,
+                      )),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -158,6 +109,119 @@ class TransactionsGrid extends StatelessWidget {
       }
     }
     return monthHeaders;
+  }
+}
+
+class DayHeadersWidget extends StatelessWidget {
+  const DayHeadersWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
+
+class MonthHeaderWidget extends StatelessWidget {
+  const MonthHeaderWidget({super.key, required this.monthHeaders});
+
+  final List<String> monthHeaders;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: monthHeaders.expand((month) {
+        return [
+          Text(
+            month,
+            style: const TextStyle(color: Colors.white),
+          ),
+          const Spacer(),
+        ];
+      }).toList(),
+    );
+  }
+}
+
+class TransactionGrid extends StatelessWidget {
+  const TransactionGrid(
+      {super.key,
+      required this.rows,
+      required this.columns,
+      required this.maxSpending,
+      required this.reversedTransactions});
+
+  final int rows;
+  final int columns;
+  final double maxSpending;
+  final List reversedTransactions;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: columns,
+        crossAxisSpacing: 2.0,
+        mainAxisSpacing: 2.0,
+      ),
+      itemCount: reversedTransactions.length,
+      itemBuilder: (context, index) {
+        int column = index % columns;
+        int row = index ~/ columns;
+        int actualIndex = row + column * rows;
+
+        if (actualIndex >= reversedTransactions.length) {
+          return Container();
+        }
+
+        Color color = _getColorForSpending(
+            reversedTransactions[actualIndex].totalAmountSpent, maxSpending);
+
+        return GestureDetector(
+          onTap: () {
+            if (reversedTransactions[actualIndex].transactions.length > 1) {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return ListView.builder(
+                    itemCount:
+                        reversedTransactions[actualIndex].transactions.length,
+                    itemBuilder: (context, innerIndex) {
+                      Transaction transaction =
+                          reversedTransactions[actualIndex]
+                              .transactions[innerIndex];
+                      return ListTile(
+                        title: Text(transaction.category),
+                        subtitle: Row(
+                          children: [
+                            transaction.isCredit
+                                ? Container()
+                                : const Text("-"),
+                            Text(transaction.amount.toStringAsFixed(2)),
+                          ],
+                        ),
+                        trailing: Text(DateFormat.yMMMMd()
+                            .format(reversedTransactions[actualIndex].date)),
+                      );
+                    },
+                  );
+                },
+              );
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text("No transaction available at the moment"),
+              ));
+            }
+          },
+          child: Container(
+            color: color,
+            margin: const EdgeInsets.all(2.0),
+          ),
+        );
+      },
+    );
   }
 
   Color _getColorForSpending(double spending, double maxSpending) {
